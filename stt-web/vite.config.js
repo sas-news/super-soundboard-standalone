58e1fa0693ce2c27e71b1f9f8c0f5b793baf8540
SAS NEWS <109570410+sas-news@users.noreply.github.com>
2025-12-20 19:33:45 +0900

    feat: `stt-web`プロジェクトを追加し、`config.json`のサウンドボードマッピングを更新

--- stt-web/vite.config.js
+++ stt-web/vite.config.js
@@ -1,9 +1,10 @@
 import path from "path";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import basicSsl from '@vitejs/plugin-basic-ssl';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), basicSsl()],
   server: {
     host: "0.0.0.0",
     port: 4173,
